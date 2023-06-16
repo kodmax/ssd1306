dbfcca687e1d65285472c4c6dc5b8739fe64f4ca
Marcin <mm.kodmax@gmail.com>
2023-06-16 16:31:45 +0200

    content

--- src/ssd1306/content/convert-l2p.ts
+++ src/ssd1306/content/convert-l2p.ts
@@ -5,15 +5,21 @@ export const convertLinearToPages = (width: number, height: number, content: Arr
     const widthInBytes = width >> 3
 
     const output = new Uint8Array(widthInBytes * height)
-    for (let page = 0; page < height; page += 8) {
-        for (let col = 0; col < widthInBytes; col++) {
+    let c = 0
+
+    for (let y = 0; y < height; y += 8) {
+        const offsetY = y * widthInBytes
+
+        for (let x = 0; x < width; x += widthInBytes) {
+            const offset = offsetY + (x >> 3)
 
             const input = new Uint8Array(8)
             for (let i = 0; i < 8; i++) {
-                input [i] = data[page + col + i * widthInBytes]
+                input[i] = data[offset + i * widthInBytes]
             }
 
-            convertBlock(input)
+            output.set(convertBlock(input), c)
+            c += 8
         }
     }
 
